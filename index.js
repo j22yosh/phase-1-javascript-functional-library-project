7b491bdcef89198be1127a7b04e6bbff0323ddb1
Joshua Welsh <j22yosh@gmail.com>
2022-05-22 23:50:53 -0400

    turning in what i have

---
+++ index.js
@@ -0,0 +1,14 @@
+const myEach = (collection, callback) => {
+    for(const c of collection){
+        callback(c)
+    }
+}
+const myMap = () => {}
+const myReduce = () => {}
+const myFind = () => {}
+const myFilter = () => {}
+const mySize = () =>{}
+const myFirst = () =>{}
+const myLast = () =>{}
+const myKeys  = () =>{}
+const myValues = () =>{}
